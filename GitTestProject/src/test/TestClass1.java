5b1243574d17a7833a821c3aec02da8ef9ebea75
gilbeee <7mini96@naver.com>
2023-01-05 23:00:34 +0900

    (23.01.05) 개발브랜치 커밋

--- GitTestProject/src/test/TestClass1.java
+++ GitTestProject/src/test/TestClass1.java
@@ -10,8 +10,11 @@ public class TestClass1 {
 		
 		for(int i = 0 ; i < 3 ; i++) {
 			c += a + i;
+			
+			a *= b;
 		}
 		
 		System.out.println(c);
+		System.out.println("master에서 변경된 소스 개발브랜치에서 수정함.");
 	}
 }
