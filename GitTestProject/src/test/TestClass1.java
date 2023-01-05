af0a09e9a6942fe2809361092bcc02c4f8d308ed
gilbeee <7mini96@naver.com>
2023-01-05 22:51:56 +0900

    (23.01.05) for문 추가

--- GitTestProject/src/test/TestClass1.java
+++ GitTestProject/src/test/TestClass1.java
@@ -3,5 +3,15 @@ package test;
 public class TestClass1 {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		
+		int a = 1;
+		int b = 3;
+		int c = 0;
+		
+		for(int i = 0 ; i < 3 ; i++) {
+			c += a + i;
+		}
+		
+		System.out.println(c);
 	}
 }
